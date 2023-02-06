8ac607a6ed1102ef91f4c9235f5c56aeab2a74fe
onalcan <onalcan20@gmail.com>
2023-02-06 22:04:47 +0100

    feat: add project dependencies and configuration variables

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,12 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val onboardingDomain = ":onboarding:onboardingDomain"
+    const val onboardingPresentation = ":onboarding:onboardingPresentation"
+
+    const val trackerData = ":tracker:trackerData"
+    const val trackerDomain = ":tracker:trackerDomain"
+    const val trackerPresentation = ":tracker:trackerPresentation"
+}
