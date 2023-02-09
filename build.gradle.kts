cfdc9cc523eb096045fb60575ef95dcfa6c743b1
onalcan <onalcan20@gmail.com>
2023-02-09 19:52:47 +0100

    code clean-up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-//plugins {
-//    id("com.android.application") version "7.4.0" apply false
-//    id("com.android.library") version "7.4.0" apply false
-//    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
-//    id("org.jetbrains.kotlin.jvm") version "1.7.21" apply false
-//    id("com.google.dagger.hilt.android") version "2.44" apply false
-//}
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
