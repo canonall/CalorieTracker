0cbb596f0cf69b6b2a1e6ce176b0508bc6e05b0b
onalcan <onalcan20@gmail.com>
2023-02-10 13:09:04 +0100

    feat: add core-ui module

---
+++ core-ui/src/main/java/com/canonal/core_ui/Dimensions.kt
@@ -0,0 +1,24 @@
+package com.canonal.core_ui
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val default: Dp = 0.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp,
+    val spaceMedium: Dp = 16.dp,
+    val spaceLarge: Dp = 32.dp,
+    val spaceExtraLarge: Dp = 64.dp
+)
+
+val LocalSpacing = compositionLocalOf { Dimensions() }
+
+val MaterialTheme.spacing: Dimensions
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSpacing.current
