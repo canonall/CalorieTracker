9f794b7f8d8396ffcaff0a53e2a76c9739039b2d
onalcan <onalcan20@gmail.com>
2023-02-10 14:42:05 +0100

    feat: add di to app module

---
+++ app/src/main/java/com/canonal/calorietracker/CalorieTrackerApp.kt
@@ -0,0 +1,8 @@
+package com.canonal.calorietracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CalorieTrackerApp: Application() {
+}
---
+++ app/src/main/java/com/canonal/calorietracker/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.canonal.calorietracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.canonal.core.data.preferences.DefaultPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        application: Application
+    ): SharedPreferences {
+        return application.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): DefaultPreferences {
+        return DefaultPreferences(sharedPref = sharedPreferences)
+    }
+}
