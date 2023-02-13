39b977698216b75f37b1017209f46b85b316993a
onalcan <onalcan20@gmail.com>
2023-02-13 21:50:27 +0100

    feat: add ui components for onboarding

---
+++ onboarding/onboarding_presentation/src/main/java/com/canonal/onboarding_presentation/components/UnitTextField.kt
@@ -0,0 +1,49 @@
+package com.canonal.onboarding_presentation.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+import com.canonal.core_ui.spacing
+
+@Composable
+fun UnitTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    unit: String,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle(
+        color = MaterialTheme.colors.primaryVariant,
+        fontSize = 70.sp
+    )
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = textStyle,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            singleLine = true,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .alignBy(LastBaseline)
+        )
+        Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceSmall))
+        Text(
+            text = unit,
+            modifier = Modifier.alignBy(LastBaseline)
+        )
+    }
+}
