d85510e5399d930516947726ab20d4ac91b4dcb1
onalcan <onalcan20@gmail.com>
2023-02-14 21:47:15 +0100

    chore: update navigation version

--- buildSrc/src/main/java/Compose.kt
+++ buildSrc/src/main/java/Compose.kt
@@ -7,7 +7,7 @@ object Compose {
     const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
     const val compiler = "androidx.compose.compiler:compiler:$composeCompilerVersion"
 
-    private const val navigationVersion = "2.4.0-beta02"
+    private const val navigationVersion = "2.5.1"
     const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
 
     private const val hiltNavigationComposeVersion = "1.0.0-beta01"
