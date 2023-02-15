6388b68159925b14c0b6feb38cb282ea3feebace
onalcan <onalcan20@gmail.com>
2023-02-15 22:36:31 +0100

    feat: add WeightScreen and WeightViewModel

---
+++ onboarding/onboarding_presentation/src/main/java/com/canonal/onboarding_presentation/weight/WeightViewModel.kt
@@ -0,0 +1,51 @@
+package com.canonal.onboarding_presentation.weight
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.canonal.core.data.preferences.DefaultPreferences
+import com.canonal.core.util.UiEvent
+import com.canonal.core.R
+import com.canonal.core.navigation.Route
+import com.canonal.core.util.UiText
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeightViewModel @Inject constructor(
+    private val preferences: DefaultPreferences
+) : ViewModel() {
+    var weight by mutableStateOf("80.0")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent: Flow<UiEvent>
+        get() = _uiEvent.receiveAsFlow()
+
+    fun onWeightEnter(weight: String) {
+        if (weight.length <= 5) {
+            this.weight = weight
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val weightAsFloat = weight.toFloatOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        message = UiText.StringResource(resId = R.string.error_weight_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveWeight(weight = weightAsFloat)
+            _uiEvent.send(UiEvent.Navigate(Route.ACTIVITY))
+        }
+    }
+}
