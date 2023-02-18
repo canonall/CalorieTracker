d5bce4d97f5693d5927a0c1165510882de10541b
onalcan <onalcan20@gmail.com>
2023-02-18 12:31:20 +0100

    feat: implement GoalTypeScreen and ViewModel

---
+++ onboarding/onboarding_presentation/src/main/java/com/canonal/onboarding_presentation/goal_type/GoalTypeScreen.kt
@@ -0,0 +1,97 @@
+package com.canonal.onboarding_presentation.goal_type
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.canonal.core.R
+import com.canonal.core.domain.model.GoalType
+import com.canonal.core.util.UiEvent
+import com.canonal.core_ui.spacing
+import com.canonal.onboarding_presentation.components.ActionButton
+import com.canonal.onboarding_presentation.components.SelectableButton
+
+@Composable
+fun GoalTypeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: GoalTypeViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { uiEvent ->
+            when (uiEvent) {
+                is UiEvent.Navigate -> onNavigate(uiEvent)
+                else -> Unit
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(MaterialTheme.spacing.spaceLarge)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.lose_keep_or_gain_weight),
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.spaceMedium))
+            Row {
+                SelectableButton(
+                    text = stringResource(id = R.string.lose),
+                    isSelected = viewModel.selectedGoalType is GoalType.LoseWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect(GoalType.LoseWeight)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.keep),
+                    isSelected = viewModel.selectedGoalType is GoalType.KeepWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect(GoalType.KeepWeight)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.gain),
+                    isSelected = viewModel.selectedGoalType is GoalType.GainWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect(GoalType.GainWeight)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+            }
+        }
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = viewModel::onNextClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
