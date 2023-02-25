c7d5c1a98a0f1f252d7d889af15dff9faf8712e9
onalcan <onalcan20@gmail.com>
2023-02-25 21:26:10 +0100

    move colors to core-ui

--- app/src/main/java/com/canonal/calorietracker/ui/theme/Theme.kt
+++ app/src/main/java/com/canonal/calorietracker/ui/theme/Theme.kt
@@ -7,8 +7,7 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
-import com.canonal.core_ui.Dimensions
-import com.canonal.core_ui.LocalSpacing
+import com.canonal.core_ui.*
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
