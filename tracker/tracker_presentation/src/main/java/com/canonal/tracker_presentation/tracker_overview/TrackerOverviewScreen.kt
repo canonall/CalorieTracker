bd7f9c3fb7c9c8f277f97a224f0ad6ad3e7f2ed8
onalcan <onalcan20@gmail.com>
2023-02-27 22:35:56 +0100

    feat-ui/ implement day selector

--- tracker/tracker_presentation/src/main/java/com/canonal/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/canonal/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,7 +1,6 @@
 package com.canonal.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -10,6 +9,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.canonal.core.util.UiEvent
 import com.canonal.core_ui.spacing
+import com.canonal.tracker_presentation.tracker_overview.components.DaySelector
 import com.canonal.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -25,8 +25,20 @@ fun TrackerOverviewScreen(
             .fillMaxSize()
             .padding(bottom = MaterialTheme.spacing.spaceMedium)
     ) {
-        item { 
+        item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = MaterialTheme.spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.spaceMedium))
         }
     }
 }
