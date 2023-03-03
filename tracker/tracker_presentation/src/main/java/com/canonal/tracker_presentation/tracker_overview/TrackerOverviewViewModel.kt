27babaeb5b0421404a0e137ae349edf2ad1998ac
onalcan <onalcan20@gmail.com>
2023-03-03 21:51:39 +0100

    fix: update food change in TrackerOverviewViewModel

--- tracker/tracker_presentation/src/main/java/com/canonal/tracker_presentation/tracker_overview/TrackerOverviewViewModel.kt
+++ tracker/tracker_presentation/src/main/java/com/canonal/tracker_presentation/tracker_overview/TrackerOverviewViewModel.kt
@@ -35,6 +35,7 @@ class TrackerOverviewViewModel @Inject constructor(
     private var getFoodsForDateJob: Job? = null
 
     init {
+        refreshFoods()
         preferences.saveShouldShowOnboarding(shouldShowOnboarding = false)
     }
 
