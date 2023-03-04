eac4a5dc87670fc70ecacef1ad9b7ffd87ae8b2f
onalcan <onalcan20@gmail.com>
2023-03-04 12:10:03 +0100

    fix: filter api results

--- tracker/tracker_data/src/main/java/com/canonal/tracker_data/repository/TrackerRepositoryImpl.kt
+++ tracker/tracker_data/src/main/java/com/canonal/tracker_data/repository/TrackerRepositoryImpl.kt
@@ -29,9 +29,23 @@ class TrackerRepositoryImpl(
                 pageSize = pageSize
             )
             Result.success(
-                searchDto.products.mapNotNull { product ->
-                    product.toTrackableFood()
-                }
+                searchDto.products
+                    .filter { product ->
+                        // if not in range, then product is not suitable as result
+                        // because it is not suitable for the app in terms of nutriments calculation
+                        // only show products that fulfill the condition
+                        // calories = (carbs * 4) + (protein * 4) + (fat * 9)
+                        val calculatedCalories =
+                            product.nutriments.carbonhydrates100g * 4f +
+                                    product.nutriments.proteins100g * 4f +
+                                    product.nutriments.fat100g * 9f
+                        val lowerBound = calculatedCalories * 0.99f
+                        val upperBound = calculatedCalories * 1.01f
+                        product.nutriments.energyKcal100g in (lowerBound..upperBound)
+                    }
+                    .mapNotNull { product ->
+                        product.toTrackableFood()
+                    }
             )
         } catch (e: Exception) {
             e.printStackTrace()
