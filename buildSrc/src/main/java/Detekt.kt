d27b92bfdb9ec64af5652aebc3db015e70e07595
onalcan <onalcan20@gmail.com>
2023-03-11 22:40:53 +0100

    add detekt to project and update rules

---
+++ buildSrc/src/main/java/Detekt.kt
@@ -0,0 +1,5 @@
+object Detekt {
+    private const val detektVersion = "1.22.0"
+    const val detekt = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$detektVersion"
+    const val detektFormatting = "io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion"
+}
